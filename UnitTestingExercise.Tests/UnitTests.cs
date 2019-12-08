7f0231e00b5110bfa4387fc1ed5de706d7b3fef6
Jay Snodderly <j.smedederly@gmail.com>
2019-12-08 11:22:02 -0600

    Unit test 1 and 2 complete with methods solved.

--- UnitTestingExercise.Tests/UnitTests.cs
+++ UnitTestingExercise.Tests/UnitTests.cs
@@ -6,51 +6,58 @@ namespace UnitTestingExercise.Tests
     public class UnitTests
     {
         [Theory]
-        [InlineData()]
+        [InlineData(1, 2, 3, 6)]
+        [InlineData(4, 5, 6, 15)]
+        [InlineData(15, 25, 35, 75)]
+        [InlineData(123, 234, 345, 702)]
         public void Add(int num1, int num2, int num3, int expected)
         {
             //Arrange
-
+            UnitTestMethods test = new UnitTestMethods();
             //Act
-
+            int actual = test.Add(num1, num2, num3);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
-        public void Subtract(int minuend, int subtrhend, int expected)
+        [InlineData(10, 5, 5)]
+        [InlineData(100, 23, 77)]
+        [InlineData(546, 231, 315)]
+        [InlineData(-1, 43, -44)]
+        [InlineData(32, -68, 100)]
+        public void Subtract(int minuend, int subtrahend, int expected)
         {
             //Arrange
-
+            UnitTestMethods test = new UnitTestMethods();
             //Act
-
+            int actual = test.Subtract(minuend, subtrahend);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(2, 6, 12)]
         public void Multiply(int num1, int num2, int expected)
         {
             //Arrange
-
+            UnitTestMethods test = new UnitTestMethods();
             //Act
-
+            int actual = test.Multiply(num1, num2);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Theory]
-        [InlineData()]
+        [InlineData(10, 2, 5)]
         public void Divide(int num1, int num2, int expected)
         {
             //Arrange
-
+            UnitTestMethods test = new UnitTestMethods();
             //Act
-
+            int actual = test.Divide(num1, num2);
             //Assert
-
+            Assert.Equal(actual, expected);
         }
 
         [Fact]
--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -3,6 +3,26 @@ namespace UnitTestingExercise
 {
     public class UnitTestMethods
     {
+        public int Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public int Subtract(int minuend, int subtrahend)
+        {
+            return minuend - subtrahend;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
         // Make sure your references are set properly
             // Righ-click on Dependencies in your .Tests project and set your reference
 
